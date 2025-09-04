66831a1d6cf7cdf512943676312b63efed3e3048
Phelps Merrell <phelps.tomas.merrell@gmail.com>
2025-09-04 00:08:37 -0600

    rmeoving junk

--- build_check.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\projects\FAANG\LookatDeezBackend\LookatDeezBackend"
-echo Building project...
-dotnet build
-echo.
-echo Build complete. Check for any remaining errors above.
-pause
